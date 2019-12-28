91f179a6b4efd6e2683e3c5d91d9f8837e8def94
dono118 <dono118@126.com>
2019-12-28 09:39:44 +0800

    domain

---
+++ interview/src/com/icode/interview/domain/Student.java
@@ -0,0 +1,12 @@
+package com.icode.interview.domain;
+
+/**
+ * @author Unicorn
+ * @create 2019-10-02 15:40
+ */
+public class Student {
+
+    public void sleep() {
+        System.out.println("sleep...");
+    }
+}
